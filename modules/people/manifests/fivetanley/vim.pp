1965eb903202a5d80aedfe43390eff794f850f49
Stanley Stuart <stanley@stan.li>
2016-02-03 16:20:53 -0600

    add plug and other vim plugins

--- modules/people/manifests/fivetanley/vim.pp
+++ modules/people/manifests/fivetanley/vim.pp
@@ -1,6 +1,31 @@
 class people::fivetanley::vim {
-  file { "$::boxen_user/.config":
+  $home = "/Users/${::boxen_user}"
+
+  file { "${home}/.config":
     ensure => directory,
     owner  => $::boxen_user,
+  }
+
+  # Install Janus
+  repository { 'janus':
+    source => 'carlhuda/janus',
+    path   => "${home}/.vim",
+  }
+  ~> exec { 'Boostrap Janus':
+    command     => 'rake',
+    cwd         => "${home}/.vim",
+    refreshonly => true,
+    environment => [
+      "HOME=${home}",
+    ],
+  }
+
+  # Install Vim-Plug
+  exec { 'download neovim':
+    # lint:ignore:80chars
+    command => "/usr/bin/curl -fLo ${home}/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+    # lint:endignore
+    creates => "${home}/.config/nvim/autoload/plug.vim"
   }
 }
